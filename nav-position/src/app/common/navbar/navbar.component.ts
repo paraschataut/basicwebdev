3f18ecb7616656641d50537a2504e468125ae12f
paraschataut <paras.chataut@live.com>
2024-08-06 02:08:47 +0545

    nav basic position

--- nav-position/src/app/common/navbar/navbar.component.ts
+++ nav-position/src/app/common/navbar/navbar.component.ts
@@ -8,16 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-    nav:string;
-    position:string;
-    constructor(){
-        this.position = "left"
-        this.nav = " text-pink-400 ";
-    }
-    ngOnInit():void{
-        if(this.position=="left"){
-            this.nav = ` text-pink-400 h-screen `;
-        }
-    }
+  nav:string;
+  nav_content:string;
+	position_left:string;
+	position_right:string;
+	position_bottom:string;
+	position_top:string;
+	// nav= nav+position_left;
+	if(postion="left"){
+	}
+  constructor(){
+    this.nav = ` fixed bg-gray-600 grid place-items-center `;
+    this.position_left=` left-0 h-screen `;
+	  this.position_right=` right-0 h-screen `;
+	  this.position_bottom=`  w-dvw bottom-0 `;
+	  this.position_top=` w-dvw top-0 `;
+    this.nav = this.nav+this.position_left;
+    this.nav_content = `  gap-6 bg-pink-300 rounded-xl `;
+    // this.nav_content = ` bg-pink-300 `;
+
+
+  }
 
 }
