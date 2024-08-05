1b6c259330b5e068cfb10eb07ffd1bfa7729751b
mira <paras.chataut@live.com>
2024-08-05 00:21:18 +0000

    new

---
+++ nav-position/src/app/funtimes/typing/typing.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-typing',
+  standalone: true,
+  imports: [],
+  templateUrl: './typing.component.html',
+  styleUrl: './typing.component.css'
+})
+export class TypingComponent {
+
+}
